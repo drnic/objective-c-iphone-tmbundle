41b8a2a2502681a07d37e11a72f69c224c8dd7e1
Dr Nic Williams <drnicwilliams@gmail.com>
2008-08-31 13:32:44 +1000

    added grammar for source.objc.iphone + Cmd for delegate protocol

--- Support/bin/delegate_protocol_snippets.rb
+++ Support/bin/delegate_protocol_snippets.rb
@@ -5,6 +5,8 @@ class DelegateProtocolSnippet
 end
 
 if __FILE__ == $0
+  require ENV['TM_SUPPORT_PATH'] + "/lib/exit_codes"
+
   puts "TODO - implement DelegateProtocolSnippet"
   TextMate.exit_discard
 end
