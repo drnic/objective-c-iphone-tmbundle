d467a6f006a46b0061a09ea8b656975b8e754d5a
Dr Nic Williams <drnicwilliams@gmail.com>
2008-08-31 14:09:17 +1000

    storing supported protocols in Support/protocols/*.yml; not generating optional methods anymore at the moment

--- Support/bin/protocol_snippets.rb
+++ Support/bin/protocol_snippets.rb
@@ -1,5 +1,7 @@
 class ProtocolSnippetNotSupported < Exception; end
 
+require "yaml"
+
 class ProtocolSnippet
   attr_reader :protocol_class
   
@@ -9,19 +11,40 @@ class ProtocolSnippet
   end
   
   def supported?
-    available = %w[UITableViewDelegate UITableViewDataSource]
-    available.include?(protocol_class)
+    protocol_definition
   end
   
   def to_s
-    header
+    header +
+    render_required_methods
   end
   
   def header
     <<-OBJC
 #pragma mark -
 #pragma mark - #{@protocol_class} methods
+
     OBJC
+  end
+  
+  def render_required_methods
+    protocol_definition["required"].inject([]) do |mem, required_method|
+      mem << <<-OBJC
+#{required_method}
+{
+  #{"$0" if mem.size == 0}
+}
+      OBJC
+      mem
+    end.join("\n")
+  end
+  
+  def protocol_definition
+    @protocol_definition ||= begin
+      file = File.dirname(__FILE__) + "/../protocols/#{protocol_class}.yml"
+      return nil unless File.exists?(file)
+      YAML.load(File.read(file))
+    end
   end
 end
 
--- Support/test/test_protocol_snippets.rb
+++ Support/test/test_protocol_snippets.rb
@@ -12,38 +12,16 @@ class TestProtocolSnippets < Test::Unit::TestCase
       expected = <<-OBJC
 #pragma mark -
 #pragma mark - UITableViewDelegate methods
+
 - (BOOL)tableView:(UITableView *)tableView shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath *)indexPath
 {
   $0
 }
 
-- (NSIndexPath *)tableView:(UITableView *)tableView
-                 targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath
-                                      toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath
+- (NSIndexPath *)tableView:(UITableView *)tableView targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath
 {
-
+  
 }
-
-// Optional UITableViewDelegate methods
-// Providing Table Cells for the Table View
-// – tableView:heightForRowAtIndexPath:
-// – tableView:indentationLevelForRowAtIndexPath:
-// – tableView:willDisplayCell:forRowAtIndexPath:
-// Managing Accessory Views
-// – tableView:accessoryTypeForRowWithIndexPath:
-// – tableView:accessoryButtonTappedForRowWithIndexPath:
-// Managing Selections
-// – tableView:willSelectRowAtIndexPath:
-// – tableView:didSelectRowAtIndexPath:
-// Modifying the Header and Footer of Sections
-// – tableView:viewForHeaderInSection:
-// – tableView:viewForFooterInSection:
-// – tableView:heightForHeaderInSection:
-// – tableView:heightForFooterInSection:
-// Editing Table Rows
-// – tableView:willBeginEditingRowAtIndexPath:
-// – tableView:didEndEditingRowAtIndexPath:
-// – tableView:editingStyleForRowAtIndexPath:
       OBJC
       assert_equal(expected, ProtocolSnippet.new("UITableViewDelegate").to_s)
     end
@@ -60,22 +38,8 @@ class TestProtocolSnippets < Test::Unit::TestCase
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-
+  
 }
-
-// Optional UITableViewDataSource methods
-// Configuring a Table View
-// – numberOfSectionsInTableView:
-// – sectionIndexTitlesForTableView:
-// – tableView:sectionForSectionIndexTitle:atIndex:
-// – tableView:titleForHeaderInSection:
-// – tableView:titleForFooterInSection:
-// Inserting or Deleting Table Rows
-// – tableView:commitEditingStyle:forRowAtIndexPath:
-// – tableView:canEditRowAtIndexPath:
-// Reordering Table Rows
-// – tableView:canMoveRowAtIndexPath:
-// – tableView:moveRowAtIndexPath:toIndexPath:
       OBJC
       assert_equal(expected, ProtocolSnippet.new("UITableViewDataSource").to_s)
     end
